bac105429beb544554287c1e8ce78956058d411d
hamzaouikhalil <hamzaouikhalil.kh@gmail.com>
2023-12-06 17:16:20 +0100

    backend admin done

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,2 +1,43 @@
 const mysql = require("mysql2");
 const connection = require("./index.js")
+
+
+module.exports = {
+    getAllUsers: function(callback){
+        const sql = `select * from users`
+        connection.query(sql,function(error,results){
+            callback(error,results)
+        })
+    },
+    getAllProducts: function(callback){
+        const sql = `select * from products`
+        connection.query(sql, function(error,results){
+            callback(error,results)
+        })
+    },
+    removeProduct: function(id,callback){
+        const sql = `delete from products where idproducts = ${id}`
+        connection.query(sql, function(error,results){
+            callback(error,results)
+        })
+    },
+    removeUser: function(id,callback){
+        const sql = `delete from users where idusers = ${id}`
+        connection.query(sql, function(error,results){
+            callback(error,results)
+        })
+    },
+    updateProduct: function(id,newPrice,callback){
+        const sql = `update products set price = ${newPrice} where idproducts = ${id}`
+        connection.query(sql, function(error,results){
+            callback(error,results)
+        })
+    },
+    addProduct: function(name,category,price,quantity,description,imageUrl,productType,callback){
+        const sql = `insert into products (name,category,price,quantity,description,imageUrl,productType) values(?,?,?,?,?,?,?)`
+        connection.query(sql,[name,category,price,quantity,description,imageUrl,productType],function(error,results){
+            callback(error,results)
+        })
+    }
+
+}
