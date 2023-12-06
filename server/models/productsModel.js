b865538f114da2622416d65449deca86ff5e0e4e
waelnasseur1 <waelnasseur6@gmail.com>
2023-12-06 16:09:23 +0100

    osssssssssssss

--- server/models/productsModel.js
+++ server/models/productsModel.js
@@ -4,7 +4,13 @@ const connection = require("./index.js");
 const getproduct = (callback) => {
   const query = "SELECT * FROM products";
 
-  return connection.query(query, callback);
+  connection.query(query, callback);
 };
 
-module.exports = { getproduct };
+const getfiltredproduct =(category,type) =>{
+    var sql = "SELECT * FROM products WHERE category = ? AND productType= ?"
+
+   return connection.promise().query(sql,[category, type])
+}
+
+module.exports = { getproduct,getfiltredproduct };
