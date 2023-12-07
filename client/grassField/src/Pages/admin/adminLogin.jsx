5169992f1574ab75b71d44d8bceca6ca13a85f32
hamzaouikhalil <hamzaouikhalil.kh@gmail.com>
2023-12-07 12:30:00 +0100

    commit to pull

---
+++ client/grassField/src/Pages/admin/adminLogin.jsx
@@ -0,0 +1,15 @@
+
+const AdminLogin = ()=>{
+  return(
+    <div>
+        hi
+        sqd
+        ssqd
+        dsqd
+        qsd
+        qdsq
+        
+    </div>
+  )
+}
+export default AdminLogin
