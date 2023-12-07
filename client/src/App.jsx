f819e5cb1f39d6e878bc455961ce0502044094ab
Maryem-Harmessi <hermassimyriam@gmail.com>
2023-12-07 15:06:03 +0100

    fix conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,8 @@ import Home from './Pages/Home';
 
 function App() {
   return (
-<<<<<<< HEAD:client/grassField/src/App.jsx
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </Router>
-  );
-=======
   <h1 className="text-3xl font-bold underline">Hello</h1>
   )
->>>>>>> 911f847f8dc02fd9eb08e45b4d9a7b2f6886ea7e:client/src/App.jsx
 }
 
 export default App;
